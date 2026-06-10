a617b0a6f3b884b47a42514f8542e270242c5ca5
AdamAh0 <adam.ahmani11@gmail.com>
2026-06-10 21:41:41 +0200

    admin changement

--- src/components/Admin/AdminAudit.js
+++ src/components/Admin/AdminAudit.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:8000/admin';
+const API_BASE = `${process.env.REACT_APP_API_URL || 'http://localhost:8000'}/admin`;
 
 export default function AdminAudit({ filters }) {
   const [logs, setLogs] = useState([]);
